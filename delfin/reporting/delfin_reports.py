bf9885395e3d27c287a292a7c0e4f71e2d889518
hmaximilian <hmaximilian496@gmail.com>
2025-09-24 11:40:18 +0000

    chore: update reporting template

--- delfin/reporting/delfin_reports.py
+++ delfin/reporting/delfin_reports.py
@@ -183,6 +183,7 @@ def generate_summary_report_DELFIN(charge, multiplicity, solvent, E_ox, E_ox_2, 
     #############################################################
     #                           -***-                           #
     #                          ComPlat                          #
+    #                        M. Hartmann                        #
     #           Karlsruhe Institute of Technology (KIT)         #
     #   Automates ORCA 6.1.0, xTB 6.7.1 and CREST 3.0.2 runs    #
     #                       Version 1.0.0                       #
